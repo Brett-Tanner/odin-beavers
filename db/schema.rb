06afbedc744987f014de504f6d759340afeda751
Brett Tanner <bretttanner171@gmail.com>
2022-11-18 22:52:44 +0900

    Seed beavers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_041440) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_123444) do
   create_table "beavers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_041440) do
     t.integer "hunger"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fav_food"
+    t.string "place_of_residence"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+max = 10
+
+Beaver.create ([{
+  name: "Reginald",
+  age: 8,
+  cuteness: max,
+  hunger: 3,
+  fav_food: "lettuce",
+  place_of_residence: "Odaiba"
+},
+{
+  name: "Shimmy",
+  age: 0.2,
+  cuteness: max,
+  hunger: 4,
+  fav_food: "Viktoria's hair",
+  place_of_residence: "Viktoria's room"
+},
+{
+  name: "Maxwell",
+  age: 12,
+  cuteness: max,
+  hunger: 1,
+  fav_food: "carrots",
+  place_of_residence: "Ueno"
+},
+{
+  name: "Shanks",
+  age: 14,
+  cuteness: max,
+  hunger: 5,
+  fav_food: "okonomiyaki",
+  place_of_residence: "Brett's house"
+}
+])
