5cb7c7f322f0909b4fcb612e86f68529a0bbd6e5
Brett Tanner <bretttanner171@gmail.com>
2022-11-18 13:23:40 +0900

    Generate Beavers

---
+++ app/controllers/beavers_controller.rb
@@ -0,0 +1,2 @@
+class BeaversController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :beavers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "beavers#index"
 end
